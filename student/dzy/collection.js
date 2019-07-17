8668347a3fbddca12e9a43c5a58fd9c925f85de4
whiiiteeeeen <37427362+whiiiteeeeen@users.noreply.github.com>
2019-07-17 17:00:58 +0800

    Update collection.js
    修改版面样式

--- student/dzy/collection.js
+++ student/dzy/collection.js
@@ -1,4 +1,4 @@
-const mymethod = require('./middleware/mymethod');							//加载中间件
+const mymethod = require('./middleware/mymethod');					//加载中间件
 
 /**********
 集合，哈希表（MAP）
@@ -11,11 +11,11 @@ const mymethod = require('./middleware/mymethod');							//加载中间件
 
 
 let arr = new ArrayList([3, 6, 5, 'xyz', 'foo', 'xyz']);				//ArrayList原型 已经在中间件里定义完成，
- 																		//直接new进行创建 arr
-arr.listAll();															//调用原型方法中的listAll来遍历集合
+ 											//直接new进行创建 arr
+arr.listAll();										//调用原型方法中的listAll来遍历集合，通过控制台显示
 
-let map0 = new Map([[1,'one'],[2,'two'],[3,'three'],[4,'four']]);		//定义一个hash(Map)数据结构
+let map0 = new Map([[1,'one'],[2,'two'],[3,'three'],[4,'four']]);			//定义一个hash(Map)数据结构
 
-map0.forEach(function(value,key,map){									//遍历 map0
+map0.forEach(function(value,key,map){							//遍历 map0
 	console.log('key:%d  value:%s',key,value);
 })
