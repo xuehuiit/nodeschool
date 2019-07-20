f041c39e12370cf8a5a2871f81bdd5233ae29118
whiiiteeeeen <37427362+whiiiteeeeen@users.noreply.github.com>
2019-07-20 12:00:06 +0800

    Update router.js
    将路由中的 路由逻辑部分，重新用控制器来异步控制

--- student/dzy/app/router.js
+++ student/dzy/app/router.js
@@ -1,133 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const mysql      = require('mysql');						//加载mysql
-let connection = mysql.createConnection({					//设置连接配置
-  host     : 'localhost',
-  user     : 'root',
-  password : '123456',
-  database : 'test'
-});
+const controller = require('./controller')    //加载控制器中间件
 
-connection.connect();
+router.get('/', controller.home);             //主页
 
+router.get('/delete',controller.deleteID);    //删除
 
+router.get('/change',controller.getChange);   //编辑
 
-router.get('/', function (req, res) {
-	let sql = 'SELECT * FROM web';
-    connection.query(sql, function (err, result) {
-    	let data = new Array();
-        if(err){
-          console.log('[SELECT ERROR] - ',err.message);
-          return;
-        }
-        for(let i =0;i<result.length;i++){
-        	data[i] = new Company();
-        	for(index in result[i]){
-        		data[i][index]=result[i][index]
-        	}
-        };
-        //data.forEach(function(item){
-        //	console.log(item.id)
-        //})
-        res.render('index.html',{
-        	data:data
-        })
-	});
-});
+router.post('/change',controller.putChange);  //提交编辑
 
-router.get('/delete',function(req,res){
-	console.log(req.query.id)
-	let delSql = 'DELETE FROM web where id='+ parseInt(req.query.id);
-	console.log(delSql)
-	connection.query(delSql,function (err, result) {
-        if(err){
-          console.log('[DELETE ERROR] - ',err.message);
-          return;
-        }        
-       console.log('id已经删除');
-       
-	res.redirect('/')
-	});
-});
-router.get('/change',function(req,res){
-	let sql = 'SELECT * FROM web WHERE id = ?';			
-	console.log(req.query.id);
-    connection.query(sql, req.query.id,function (err, result) {
-    	let data = new Array();
-        if(err){
-          console.log('[SELECT ERROR] - ',err.message);
-          return;
-        }
-        for(let i =0;i<result.length;i++){
-        	data[i] = new Company();
-        	for(index in result[i]){
-        		data[i][index]=result[i][index]
-        	}
-        };
-        //data.forEach(function(item){
-        //	console.log(item.id)
-        //})
-        res.render('change.html',{
-        	data:data[0]
-        })
-	});
-	
-});
-router.post('/change',function(req,res){
-	console.log(req.body);
-	var modSql = 'UPDATE web SET name = ?,url = ?,country = ? WHERE Id = ?';//定义更改数据的模板
-	var modSqlParams = [req.body.name, req.body.url,req.body.country,req.body.id];		//定义更改的数据变量
-	
-	connection.query(modSql,modSqlParams,function (err, result) {
-	   if(err){
-	         console.log('[UPDATE ERROR] - ',err.message);
-	         return;
-	   }
-	   console.log('修改成功成功') 
-	   res.redirect('/')
-	});
-});
-router.get('/add',function(req,res){
-	res.render('add.html')
-})
-router.post('/add',function(req,res){
-	let  addSql = 'INSERT INTO web(Id,name,url,country) VALUES(0,?,?,?)';	//定义增加数据的模板，id为自加
-	let  addSqlParams = [req.body.name, req.body.url,req.body.country];				//增加的数据
-
-	connection.query(addSql,addSqlParams,function (err, result) {	
-	        if(err){
-	         console.log('[INSERT ERROR] - ',err.message);
-	         return;
-	        }      
-	        console.log('添加成功')  
-	 	res.redirect('/')
-	});
-})
-router.post('/search',function(req,res){
-	let sql1 = 'SELECT * FROM web WHERE name = ?';				//定义两种查询方式
-	connection.query(sql1,req.body.name, function (err, result) {
-		let data = new Array();
-        if(err){
-          console.log('[SELECT ERROR] - ',err.message);
-          return;
-        }
-        for(let i =0;i<result.length;i++){
-        	data[i] = new Company();
-        	for(index in result[i]){
-        		data[i][index]=result[i][index]
-        	}
-        };  
-        res.render('search.html',{
-        	data:data
-        })
-	});
-})
+router.get('/add',controller.getAdd)          //增加
+router.post('/add',controller.putAdd)         //提交增加
+router.post('/search',controller.putSearch)   //搜索
 
 module.exports = router;
-
-function Company(){
-	this.id = null;
-	this.name = null;
-	this.url = null;
-	this.country = null;
-}
