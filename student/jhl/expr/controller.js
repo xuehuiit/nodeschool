854957dce2b25cdbba7cdb9209eccf6ebf5a202e
joiller <781225147@qq.com>
2019-07-26 13:17:22 +0800

    day5

--- student/jhl/expr/controller.js
+++ student/jhl/expr/controller.js
@@ -1,5 +1,6 @@
 const User = require('./db')
 
+// 查询全部并返回页面
 async function findAll(req,res){
     await User.findAll()
         .then(value => {
@@ -11,6 +12,7 @@ async function findAll(req,res){
         })
 }
 
+// 查询个人，返回个人信息页面
 async function findOne(req,res){
     await User.findOne({
         where: {
@@ -18,18 +20,41 @@ async function findOne(req,res){
         }
     })
         .then(value => {
-            res.render('main.html',{users:value.dataValues})
+            res.render('profile.html',{user:value.dataValues})
         })
 }
 
+// 增加数据并转至主页面
 async function create(req,res) {
     console.log(req.body)
     await User.create(req.body)
-    await findAll(req,res)
+    await res.redirect('/users/more/find')
+}
+
+// 删除数据并转至主页面
+async function destroy(req,res) {
+    await User.destroy({
+        where: req.body
+    })
+    await res.redirect('/users/more/find')
+}
+
+// 更新数据并转至主页面
+async function update(req,res){
+    await User.update(req.body,{
+        where: {
+            id: req.params.user_id
+        }
+    })
+        .then(value => {
+            res.redirect('/users/more/find')
+        })
 }
 
 module.exports = {
     findAll,
     findOne,
-    create
+    create,
+    destroy,
+    update
 }
--- student/jhl/expr/routes.js
+++ student/jhl/expr/routes.js
@@ -2,18 +2,30 @@ const express = require('express'),
     router = express.Router(),
     controller = require('./controller')
 
-router.route('/one/:user_id')
-    .get(async (req,res)=>{
-        console.log(req.params)
-        await controller.findOne(req,res)
-    })
+// 查询全部数据
+router.get('/more/find',async function (req,res) {
+    await controller.findAll(req,res)
+})
 
+// 增加数据
 router.post('/create',async (req,res)=>{
     await controller.create(req,res)
 })
 
-router.get('/more/find',async function (req,res) {
-    await controller.findAll(req,res)
+// 删除数据
+router.post('/delete',async (req,res)=>{
+    await controller.destroy(req,res)
+})
+
+// 个人信息页面
+router.route('/profile/:user_id')
+    .get(async (req,res)=>{
+        await controller.findOne(req,res)
+    })
+
+// 修改数据
+router.post('/modify/:user_id',async (req,res)=>{
+    await controller.update(req,res)
 })
 
 module.exports = router
